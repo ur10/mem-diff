74d3bd59fd54634188c3f2db265d05aa10075450
UR10 <utkarsh.rai60@gmail.com>
2025-08-17 21:40:49 -0400

    Added a working enb

--- diffusion_policy/env/pusht/multi_push.py
+++ diffusion_policy/env/pusht/multi_push.py
@@ -1,13 +1,18 @@
 import numpy as np
-from diffusion_policy.env.pusht.pymunk_override import DrawOptions
+# from diffusion_policy.env.pusht.pymunk_override import DrawOptions
 import gym
 from gym import spaces
 import pygame
 import pymunk
 import pymunk.pygame_util
 from pymunk.vec2d import Vec2d
+import cv2
+from pymunk_override import DrawOptions
 
 class MultiPushEnv(gym.Env):
+    metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 10}
+    reward_range = (0., 1.)
+
     def __init__(self,            
             legacy=False, 
             block_cog=None, damping=None,
@@ -29,6 +34,7 @@ class MultiPushEnv(gym.Env):
         self.render_size = render_size
         self.sim_hz = 100
         self.red_done = False
+        self.goal_poses = [(80,200), (180,200), (280, 200)]
         # Local controller params.
         self.k_p, self.k_v = 100, 20    # PD control.z
         self.control_hz = self.metadata['video.frames_per_second']
@@ -72,9 +78,11 @@ class MultiPushEnv(gym.Env):
         self.latest_action = None
         self.reset_to_state = reset_to_state
 
+
     def reset(self):
-        self.blocks = np.random.rand(self.num_blocks, 2) * self.render_size
-        return self.blocks.flatten()
+        # self.blocks = np.random.rand(self.num_blocks, 2) * self.render_size
+        self._setup()
+        # return self.blocks.flatten()
     
     def _add_segment(self, a, b, radius):
         shape = pymunk.Segment(self.space.static_body, a, b, radius)
@@ -115,5 +123,65 @@ class MultiPushEnv(gym.Env):
             self._add_segment((5, 506), (506, 506), 2)
         ]
         self.space.add(*walls)
-        self.add_box((250, 300), 20,20)
+        self.add_box((150, 300), 40,40)
+        self.add_box((250, 300), 40,40)
+        self.add_box((350, 300), 40,40) # Add different color names down the line
         self.add_circle((250, 350),15)
+
+    def render_frame(self,mode):
+        if self.window is None and mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode((self.window_size, self.window_size))
+        if self.clock is None and mode == "human":
+            self.clock = pygame.time.Clock()
+
+        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas.fill((255, 255, 255))
+        self.screen = canvas
+
+        draw_options = DrawOptions(canvas)
+
+
+        # Draw the 3 goal poses.
+        for goal_pose in self.goal_poses:
+            print(goal_pose)
+            rect = pygame.Rect(goal_pose[0]-50, goal_pose[1] - 50,50,50)
+            pygame.draw.rect(canvas, (255, 165, 0), rect=rect)
+
+        # Draw agent and block.
+        self.space.debug_draw(draw_options)
+
+        if mode == "human":
+            # The following line copies our drawings from `canvas` to the visible window
+            self.window.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+
+            # the clock is already ticked during in step for "human"
+
+
+        img = np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
+        img = cv2.resize(img, (self.render_size, self.render_size))
+        if self.render_action:
+            if self.render_action and (self.latest_action is not None):
+                action = np.array(self.latest_action)
+                coord = (action / 512 * 96).astype(np.int32)
+                marker_size = int(8/96*self.render_size)
+                thickness = int(1/96*self.render_size)
+                cv2.drawMarker(img, coord,
+                    color=(255,0,0), markerType=cv2.MARKER_CROSS,
+                    markerSize=marker_size, thickness=thickness)
+        return img
+
+
+def main():
+    push_env = MultiPushEnv()
+    while True:
+        push_env.reset()
+        push_env.render_frame(mode="human")
+
+if __name__=="__main__":
+    main()
